37b6643d2d251d80fe5cb4989601f7c6f727723c
Mazaev Andrey <mazaev2000@gmail.com>
2022-12-19 00:10:05 +0300

    feat: create register post request

---
+++ server/api/auth/register.post.js
@@ -0,0 +1,14 @@
+import { sendError } from 'h3'
+export default defineEventHandler(async (event) => {
+  const body = await readBody(event)
+
+  const { username, email, password, repeatPassword, name } = body
+
+  if (!username || !email || !password || !repeatPassword || !name) {
+    return sendError(event, createError({ statusCode: 400, statusMessage: 'Invalid params' }))
+  }
+
+  return {
+    body: body
+  }
+})
