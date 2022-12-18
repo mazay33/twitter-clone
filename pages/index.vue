20d826ed312e0cd3f0aa56d1828334fe347f936d
Mazaev Andrey <mazaev2000@gmail.com>
2022-12-18 23:35:58 +0300

    feat: create MainSection

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <MainSection title="Home" :loading="loading">
+      this is our home page
+    </MainSection>
+  </div>
+</template>
+
+<script setup>
+const loading = ref(false)
+</script>
