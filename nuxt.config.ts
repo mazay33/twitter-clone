7a94ff60deb3225920da458face5f2d0f2bab12f
Mazaev Andrey <mazaev2000@gmail.com>
2022-12-21 23:35:46 +0300

    feat: jwt auth

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss'],
+
+
+  runtimeConfig: {
+    jwtAccessSecret: process.env.JWT_ACCESS_TOKEN_SECRET,
+    jwtRefreshSecret: process.env.JWT_REFRESH_TOKEN_SECRET,
+
+  }
 })
--- server/api/auth/login.post.js
+++ server/api/auth/login.post.js
@@ -1,6 +1,10 @@
 
 import { getUserByUsername } from "../../db/users.js"
 import bcrypt from "bcrypt"
+import { generateTokens, sendRefreshToken } from "~~/server/utils/jwt.js"
+import { userTransformer } from "~~/server/transformers/user.js"
+import { createRefreshToken } from "~~/server/db/resfreshTokens.js"
+
 export default defineEventHandler(async (event) => {
   const body = await readBody(event)
 
@@ -24,10 +28,24 @@ export default defineEventHandler(async (event) => {
 
   const doesThePasswordMatch = await bcrypt.compare(password, user.password)
 
-  const {accessToken, refreshToken} = generateTokens
+  if (!doesThePasswordMatch) {
+    return sendError(event, createError({
+      statusCode: 400,
+      statusMessage: 'Username or password is invalid'
+    }))
+  }
+
+  const { accessToken, refreshToken } = generateTokens(user)
+
+  await createRefreshToken({
+    token: refreshToken,
+    userId: user.id
+  })
+
+  sendRefreshToken(event, refreshToken)
 
   return {
-    user: user,
-    doesThePasswordMatch
+    accessToken: accessToken,
+    user: userTransformer(user),
   }
 })
