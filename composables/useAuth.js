9824d52bdd5020a1e7d0fdbe4a7c91205ee47fdb
Mazaev Andrey <mazaev2000@gmail.com>
2022-12-24 23:59:44 +0300

    feat: added useAuth

---
+++ composables/useAuth.js
@@ -0,0 +1,66 @@
+export default () => {
+  const useAuthToken = () => useState('auth_token')
+  const useAuthUser = () => useState('auth_user')
+
+  const setToken = (newToken) => {
+    const authToken = useAuthToken()
+    authToken.value = newToken
+  }
+
+  const setUser = (newUser) => {
+    const authUser = useAuthUser()
+    authUser.value = newUser
+  }
+
+  const login = ({ username, password }) => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const data = await $fetch('/api/auth/login', {
+          method: 'POST',
+          body: {
+            username,
+            password
+          }
+        })
+
+        setToken(data.access_token)
+        setUser(data.user)
+
+        resolve(true)
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
+
+  const refreshToken = () => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const data = await $fetch('/api/auth/refresh')
+
+        setToken(data.access_token)
+        resolve(true)
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
+
+  const initAuth = () => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        await refreshToken()
+        await getUser()
+        resolve(true)
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
+
+  return {
+    login,
+    useAuthUser
+  }
+
+}
