a74b2aa7ff8570698cf2927bc07dd0fcd6eb5bd1
Mazaev Andrey <mazaev2000@gmail.com>
2022-12-24 23:59:14 +0300

    feat: getRefreshTokenByToken

--- server/db/resfreshTokens.js
+++ server/db/resfreshTokens.js
@@ -2,6 +2,14 @@ import { prisma } from "."
 
 export const createRefreshToken = (refreshToken) => {
   return prisma.refreshToken.create({
-      data: refreshToken
+    data: refreshToken
+  })
+}
+
+export const getRefreshTokenByToken = (token) => {
+  return prisma.refreshToken.findUnique({
+    where: {
+      token
+    }
   })
 }
