4c79709bbe024f30882058df7add620c97df3916
Mazaev Andrey <mazaev2000@gmail.com>
2022-12-24 23:58:02 +0300

    feat: added autPage

--- app.vue
+++ app.vue
@@ -1,12 +1,13 @@
 <template>
-  <div :class="{ 'dark': darkMode }">
+  <div :class="{ dark: darkMode }">
     <div class="bg-white dark:bg-dim-900">
-      <div class="min-h-full">
 
-        <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
 
+      <!-- App -->
+      <div v-if="user" class="min-h-full">
+        <div class="mx-auto grid grid-cols-12 sm:px-6 lg:max-w-7xl lg:gap-5 lg:px-8">
           <!-- Left sidebar -->
-          <div class="hidden md:block xs-col-span-1 xl:col-span-2">
+          <div class="xs-col-span-1 hidden md:block xl:col-span-2">
             <div class="sticky top-0">
               <SidebarLeft />
             </div>
@@ -18,25 +19,23 @@
           </main>
 
           <!-- Right Sidebar -->
-          <div class="hidden col-span-12 md:block xl:col-span-4 md:col-span-3">
+          <div class="col-span-12 hidden md:col-span-3 md:block xl:col-span-4">
             <div class="sticky top-0">
               <SidebarRight />
             </div>
           </div>
-
-
-
         </div>
-
       </div>
 
-      
+      <AuthPage v-else />
+
+
     </div>
   </div>
-
-
 </template>
 
 <script setup>
-const darkMode = ref(false)
+const darkMode = ref(false);
+const {useAuthUser} = useAuth()
+const user = useAuthUser()
 </script>
---
+++ components/Auth/Form.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="w-full">
+      <div class="flex justify-center">
+          <div class="w-10 h-10">
+              <LogoTwitter />
+          </div>
+      </div>
+
+      <div class="pt-5 space-y-6">
+
+          <UIInput v-model="data.username" label="Username" placeholder="@username" />
+
+          <UIInput label="Password" placeholder="********" type="password" v-model="data.password" />
+
+
+          <div @click="handleLogin">
+              Login
+          </div>
+
+      </div>
+  </div>
+</template>
+<script setup>
+const data = reactive({
+  username: '',
+  password: '',
+  loading: false
+})
+async function handleLogin() {
+  const { login } = useAuth()
+  data.loading = true
+  try {
+      await login({
+          username: data.username,
+          password: data.password
+      })
+  } catch (error) {
+      console.log(error)
+  } finally {
+      data.loading = false
+  }
+}
+const isButtonDisabled = computed(() => {
+  return (!data.username || !data.password) || data.loading
+})
+</script>
